3077da6c81e46d4414186778b408f91152aa2c07
Sara Ghodsi <55705790+saraghds@users.noreply.github.com>
2024-05-09 18:24:20 -0400

    chore: revert logs

--- cmd/tsbs_run_queries_clickhouse/main.go
+++ cmd/tsbs_run_queries_clickhouse/main.go
@@ -84,9 +84,7 @@ func getConnectString(workerNumber int) string {
 	// Round robin the host/worker assignment by assigning a host based on workerNumber % totalNumberOfHosts
 	host := hostsList[workerNumber%len(hostsList)]
 
-	output := fmt.Sprintf("tcp://%s:9000?username=%s&password=%s&database=%s", host, user, password, runner.DatabaseName())
-	fmt.Printf("clickhouse host=%s\n", output)
-	return output
+	return fmt.Sprintf("tcp://%s:9000?username=%s&password=%s&database=%s", host, user, password, runner.DatabaseName())
 }
 
 // prettyPrintResponse prints a Query and its response in JSON format with two
