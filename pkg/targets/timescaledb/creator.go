c10393f748e927a481e964d2e56535abd1484db5
Sara Ghodsi <55705790+saraghds@users.noreply.github.com>
2024-05-10 13:07:43 -0400

    test: logs

--- pkg/targets/timescaledb/creator.go
+++ pkg/targets/timescaledb/creator.go
@@ -59,6 +59,7 @@ func MustConnect(dbType, connStr string) *sql.DB {
 
 func (d *dbCreator) DBExists(dbName string) bool {
 	db := MustConnect(d.driver, d.connStr)
+	fmt.Printf("d.connStr=%s\n", d.connStr)
 	defer db.Close()
 	r := MustQuery(db, "SELECT 1 from pg_database WHERE datname = $1", dbName)
 	defer r.Close()
