5e51cf40c658125e9bf1e70423ab58ca91656d02
Sara Ghodsi <55705790+saraghds@users.noreply.github.com>
2024-05-16 11:35:51 -0400

    feat: flightsql

--- cmd/tsbs_run_queries_influxdb3/main.go
+++ cmd/tsbs_run_queries_influxdb3/main.go
@@ -7,25 +7,32 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"time"
 
 	"github.com/InfluxCommunity/influxdb3-go/influxdb3"
+	"github.com/apache/arrow/go/v10/arrow/flight/flightsql"
 	"github.com/blagojts/viper"
 	"github.com/spf13/pflag"
 	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases"
 	"github.com/timescale/tsbs/internal/utils"
 	"github.com/timescale/tsbs/pkg/query"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
 
 // Program option vars:
 var (
-	host     string
-	port     string
-	token    string
-	bucket   string
-	database string
-	secure   bool
+	host      string
+	port      string
+	token     string
+	bucket    string
+	database  string
+	secure    bool
+	flightSQL bool
 )
 
 // Global vars:
@@ -44,6 +51,7 @@ func init() {
 	pflag.String("bucket", "", "Bucket name for serverless")
 	pflag.String("database", "", "Database name for dedicated")
 	pflag.Bool("secure", false, "Secure transport credentials")
+	pflag.Bool("flightsql", false, "Using FlightSQL")
 
 	pflag.Parse()
 
@@ -63,6 +71,7 @@ func init() {
 	bucket = viper.GetString("bucket")
 	database = viper.GetString("database")
 	secure = viper.GetBool("secure")
+	flightSQL = viper.GetBool("flightsql")
 
 	runner = query.NewBenchmarkRunner(config)
 }
@@ -72,19 +81,33 @@ func main() {
 }
 
 type processor struct {
-	client *influxdb3.Client
+	client          *influxdb3.Client
+	flightSqlClient *flightsql.Client
 }
 
 func newProcessor() query.Processor { return &processor{} }
 
 func (p *processor) Init(workerNumber int) {
+	hostPort := fmt.Sprintf("%s:%s", host, port)
+
 	client, err := influxdb3.New(influxdb3.ClientConfig{
-		Host:     fmt.Sprintf("%s:%s", host, port),
+		Host:     hostPort,
 		Token:    token,
 		Database: database,
 	})
 	databases.PanicIfErr(err)
 	p.client = client
+
+	var dialOpt grpc.DialOption
+	if secure {
+		clientCreds := credentials.NewTLS(&tls.Config{})
+		dialOpt = grpc.WithTransportCredentials(clientCreds)
+	} else {
+		dialOpt = grpc.WithTransportCredentials(insecure.NewCredentials())
+	}
+	flightSqlClient, err := flightsql.NewClient(hostPort, nil, nil, dialOpt)
+	databases.PanicIfErr(err)
+	p.flightSqlClient = flightSqlClient
 }
 
 func (p *processor) ProcessQuery(q query.Query, _ bool) ([]*query.Stat, error) {
@@ -92,15 +115,36 @@ func (p *processor) ProcessQuery(q query.Query, _ bool) ([]*query.Stat, error) {
 	start := time.Now()
 	qry := string(tq.SqlQuery)
 
-	iterator, err := p.client.Query(context.Background(), qry)
-	databases.PanicIfErr(err)
+	if flightSQL {
+		ctx := context.Background()
+		ctx = metadata.AppendToOutgoingContext(ctx, "authorization", fmt.Sprintf("Token %s", token))
+		ctx = metadata.AppendToOutgoingContext(ctx, "database", database)
+		flightInfo, err := p.flightSqlClient.Execute(ctx, qry)
+		databases.PanicIfErr(err)
 
-	output := ""
-	for iterator.Next() {
-		value := iterator.Value()
-		output += fmt.Sprintf("%s\n", fmt.Sprint(value))
+		output := ""
+		for _, endpoint := range flightInfo.Endpoint {
+			flightReader, err := p.flightSqlClient.DoGet(ctx, endpoint.Ticket)
+			databases.PanicIfErr(err)
+			for flightReader.Next() {
+				record := flightReader.Record()
+				output += fmt.Sprintf("%v\n", record)
+			}
+			flightReader.Release()
+		}
+
+		fmt.Printf("%s\n\n%s\n-----\n\n", qry, output)
+	} else {
+		iterator, err := p.client.Query(context.Background(), qry)
+		databases.PanicIfErr(err)
+
+		output := ""
+		for iterator.Next() {
+			value := iterator.Value()
+			output += fmt.Sprintf("%s\n", fmt.Sprint(value))
+		}
+		fmt.Printf("%s\n\n%s\n-----\n\n", qry, output)
 	}
-	fmt.Printf("%s\n\n%s\n-----\n\n", qry, output)
 
 	took := float64(time.Since(start).Nanoseconds()) / 1e6
 	stat := query.GetStat()
